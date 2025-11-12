fbde562482d1a1b85380ee90f660cb9143da2b83
Joaquin Rodriguez <ertsyvler@gmail.com>
2025-11-12 19:43:57 -0300

    arrancando CarritoNegocio

--- Negocio/CarritoNegocio.cs
+++ Negocio/CarritoNegocio.cs
@@ -9,5 +9,38 @@ namespace Negocio
 {
     public class CarritoNegocio
     {
+        public List<Producto> listarCarritoUsuario(int id)
+        {
+
+            // primero consulta a carrito para buscar id de productos en el carrito del usuario
+            var datos = new AccesoDatos();
+
+            try
+            {
+                string consulta = "SELECT DISTINCT(IdProducto), Cantidad FROM Carrito WHERE IdUsuario = @id;";
+                datos.setConsulta(consulta);
+                datos.setearParametro("@id", id);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    List<int> idsProductos = new List<int>();
+                    idsProductos.Add((int)datos.Lector["IdProducto"]);
+                    // producto.Cantidad = (int)datos.Lector["Cantidad"];
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            // ya con los id de productos, hacer otra consulta para traer los productos completos como en el catalogo pero filtrados por esos id
+
+            var lista = new List<Producto>();
+
+
+            return lista;
+        }
     }
 }
