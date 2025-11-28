d180ea5556e3869e2b62a5b4c566fdae0b77fbbf
Joaquin Rodriguez <ertsyvler@gmail.com>
2025-11-28 19:17:34 -0300

    clases venta

---
+++ Domio/Venta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Venta
+    {
+        public int Id { get; set; }
+        public int IdCliente { get; set; }
+        public DateTime Fecha { get; set; }
+        public string NumeroFactura { get; set; }
+
+        // Composiciones
+        public Usuario Cliente { get; set; }
+        public List<VentaDetalle> Detalles { get; set; }
+
+        public decimal Total
+        {
+            get
+            {
+                return Detalles?.Sum(d => d.Subtotal) ?? 0;
+            }
+        }
+    }
+}
---
+++ Domio/VentaDetalle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class VentaDetalle
+    {
+        public int Id { get; set; }
+        public int IdVenta { get; set; }
+        public int IdProducto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+
+        // Composiciones
+        public Producto Producto { get; set; }
+
+        public decimal Subtotal
+        {
+            get { return PrecioUnitario * Cantidad; }
+        }
+    }
+}
